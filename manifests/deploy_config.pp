8ccd0565c9afdee9dd9d916063a98c209940716d
varac <varacanero@zeromail.org>
2012-11-01 09:32:51 +0100

    typo in filename, mode 660

--- manifests/deploy_config.pp
+++ manifests/deploy_config.pp
@@ -1,11 +1,11 @@
 class couchdb::deploy_config {
-  file { '/etc/couchdb/local_ini':
+  file { '/etc/couchdb/local.ini':
     source  => [ "puppet:///modules/site_couchdb/${::fqdn}/local.ini",
                 'puppet:///modules/site_couchdb/local.ini',
                 'puppet:///modules/couchdb/local.ini' ],
     notify  => Service[couchdb],
     owner   => couchdb,
     group   => couchdb,
-    mode    => '0600'
+    mode    => '0660'
   }
 }
