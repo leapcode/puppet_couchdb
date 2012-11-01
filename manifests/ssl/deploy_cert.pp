293e609c70157cbe73e9a7962b6bc9b5393b3778
varac <varacanero@zeromail.org>
2012-11-01 09:53:50 +0100

    provide path to ssl cert file definitions

--- manifests/ssl/deploy_cert.pp
+++ manifests/ssl/deploy_cert.pp
@@ -1,20 +1,23 @@
 define couchdb::ssl::deploy_cert ($cert, $key) {
 
-  file { $couchdb::cert_path:
+  file { 'couchdb_cert_directory':
+    path    => "$couchdb::cert_path:",
     ensure  => 'directory',
     mode    => '0600',
     owner   => 'couchdb',
     group   => 'couchdb';
   }
 
-  file { "$couchdb::cert_path/server_cert.pem":
+  file { 'couchdb_cert"':
+    path    => "$couchdb::cert_path/server_cert.pem",
     mode    => '0644',
     owner   => 'couchdb',
     group   => 'couchdb',
     content => $cert
   }
 
-  file { "$couchdb::cert_path/server_key.pem":
+  file { 'couchdb_key':
+    path    => "$couchdb::cert_path/server_key.pem",
     mode    => '0600',
     owner   => 'couchdb',
     group   => 'couchdb',
