110fed8abd8c2d7ef4f73bd1a6d0e0f3665190cf
varac <varacanero@zeromail.org>
2012-11-03 21:51:10 +0100

    site_couchdb:: -> couchdb::

--- manifests/add_user.pp
+++ manifests/add_user.pp
@@ -1,15 +1,9 @@
 define couchdb::add_user ($host, $roles, $pw ) {
-
-  #exec { "/usr/bin/curl -X PUT $host/_users/org.couchdb.user:$name --data \'{ \"_id\": \"org.couchdb.user:$name\", \"type\": \"user\", \"name\": \"$name\", \"roles\": $roles, \"password\": \"$pw\"}\'":
-  #  unless => "/usr/bin/curl -s -X GET $host/_users/org.couchdb.user:$name| grep -qv error",
-  #}
-
-  site_couchdb::query { "create_user_$name":
+  couchdb::query { "create_user_$name":
     cmd  => 'PUT',
     host => $host,
     url  => "_users/org.couchdb.user:$name",
     data => "{ \"_id\": \"org.couchdb.user:$name\", \"type\": \"user\", \"name\": \"$name\", \"roles\": $roles, \"password\": \"$pw\"}",
   }
-
 }
 
--- manifests/create_db.pp
+++ manifests/create_db.pp
@@ -2,15 +2,15 @@ define couchdb::create_db ($host,
                                 $admins="{\"names\": [], \"roles\": [] }", 
                                 $readers="{\"names\": [], \"roles\": [] }") {
 
-  Site_couchdb::Query["create_db_$name"] -> Site_couchdb::Query["db_security_${name}"]
+  Couchdb::Query["create_db_$name"] -> Couchdb::Query["db_security_${name}"]
 
-  site_couchdb::query { "create_db_$name":
+  couchdb::query { "create_db_$name":
     cmd  => 'PUT',
     host => $host,
     url  => $name,
   }
 
-  site_couchdb::query { "db_security_${name}":
+  couchdb::query { "db_security_${name}":
     cmd  => 'PUT',
     host => $host,
     url  => "$name/_security",
