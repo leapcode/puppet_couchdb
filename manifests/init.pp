753b7175e6ea27b5feeaf2216d3075d946da31c2
varac <varacanero@zeromail.org>
2013-01-20 12:54:20 +0100

    re-add wheezy suppport

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class couchdb {
   case $::operatingsystem {
     Debian: {
       case $::lsbdistcodename {
-        /lenny|squeeze/: { include couchdb::debian }
+        /lenny|squeeze|wheezy/: { include couchdb::debian }
         default:         { fail "couchdb not available for ${::operatingsystem}/${::lsbdistcodename}" }
       }
     }
