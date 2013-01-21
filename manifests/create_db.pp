dcb8a082ac842b0660819ea61f9448c4e373746e
varac <varacanero@zeromail.org>
2013-01-21 18:05:19 +0100

    towards create_db using new update script

--- manifests/create_db.pp
+++ manifests/create_db.pp
@@ -1,6 +1,6 @@
-define couchdb::create_db ($host='127.0.0.1:5984', 
-                                $admins="{\"names\": [], \"roles\": [] }", 
-                                $readers="{\"names\": [], \"roles\": [] }") {
+define couchdb::create_db ( $host='127.0.0.1:5984',
+                            $admins='{\'names\': [], \'roles\': [] }',
+                            $readers='{\'names\': [], \'roles\': [] }') {
 
   Couchdb::Query["create_db_$name"] -> Couchdb::Query["db_security_${name}"]
 
@@ -16,5 +16,18 @@ define couchdb::create_db ($host='127.0.0.1:5984',
     url  => "$name/_security",
     data => "{ \"admins\": $admins, \"readers\": $readers }"
   }
+
+  #couchdb::update { "create_db_$name":
+  #  db    => $name,
+  #  id    => '',
+  #  data  => ''
+  #}
+
+  #couchdb::update { "db_security_$name":
+  #  db    => $name,
+  #  id    => '_security',
+  #  data => "{ \"admins\": $admins, \"readers\": $readers }"
+  #}
+
 
 }
