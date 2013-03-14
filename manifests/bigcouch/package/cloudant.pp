77b80561e187c93f7c48f5ac4136e800702b0cec
varac <varacanero@zeromail.org>
2013-03-14 15:35:22 +0100

    cloudant now have signed packages

--- manifests/bigcouch/package/cloudant.pp
+++ manifests/bigcouch/package/cloudant.pp
@@ -1,19 +1,26 @@
 class couchdb::bigcouch::package::cloudant {
 
-  # cloudant only provides unauthenticated packages
-  # use this if you need bigcouch 0.4.0
+
+  # cloudants signing key can be fetched from
+  # http://packages.cloudant.com/KEYS, please use the apt module to
+  # distribute it on your servers
+  # It's verified fingerprint is:
+  # BAF9 B315 D438 5FB9 B5DE 334B 59E0 1FBD 15BE 8E26
 
   apt::sources_list {'bigcouch-cloudant.list':
-    content => "deb http://packages.cloudant.com/debian $::lsbdistcodename main"
+    content => 'deb http://packages.cloudant.com/debian squeeze main'
   }
 
-  # currently, there's no other way with puppet to install unauthenticated
-  # pacakges: http://projects.puppetlabs.com/issues/556
-  # so we need to globally allow apt to install unauthenticated
-  # packages.
-
-  apt::apt_conf { 'allow_unauthenticated':
-    content => 'APT::Get::AllowUnauthenticated yes;',
+  # right now, cloudant only provides authenticated ibigcouch 0.4.2 packages
+  # for squeeze, therefore we need to include squeeze repo for depending 
+  # packages
+  if $::lsbdistcodename == 'wheezy' {
+    apt::sources_list {'squeeze.list':
+      content => 'deb http://http.debian.net/debian squeeze main
+deb http://security.debian.org/ squeeze/updates main
+',
+      before  => Exec[refresh_apt]
+    }
   }
 
 }
