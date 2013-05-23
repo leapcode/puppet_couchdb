0c34d9bd1c51d4e633f92e459b7973008350aefb
varac <varacanero@zeromail.org>
2013-05-23 21:20:07 +0200

    use {} when no data is passed

--- manifests/query.pp
+++ manifests/query.pp
@@ -1,4 +1,4 @@
-define couchdb::query ( $cmd, $url, $host='127.0.0.1:5984', $data = '' ) {
+define couchdb::query ( $cmd, $url, $host='127.0.0.1:5984', $data = '{}' ) {
 
   exec { "/usr/bin/curl --netrc-file /etc/couchdb/couchdb.netrc -X ${cmd} ${host}/${url} --data \'${data}\'":
     require => [ Package['curl'], Exec['wait_for_couchdb'] ]
