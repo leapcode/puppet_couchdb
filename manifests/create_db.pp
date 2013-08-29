61eca267cb52252371a71926a64d7f3b7350489d
varac <varacanero@zeromail.org>
2013-08-29 12:16:01 +0200

    puppet couchdb module doesn't create necessary databases anymore (Bug #3594)

--- manifests/create_db.pp
+++ manifests/create_db.pp
@@ -1,15 +1,16 @@
-define couchdb::create_db ( $host='127.0.0.1:5984',
-                            $admins="{\"names\": [], \"roles\": [] }",
-                            $readers="{\"names\": [], \"roles\": [] }" )
+define couchdb::create_db (
+  $host='127.0.0.1:5984',
+  $admins="{\"names\": [], \"roles\": [] }",
+  $readers="{\"names\": [], \"roles\": [] }" )
 {
 
   Couchdb::Query["create_db_${name}"] -> Couchdb::Query["db_security_${name}"]
 
   couchdb::query { "create_db_${name}":
-    cmd  => 'PUT',
-    host => $host,
-    url  => $name,
-    unless => "/usr/bin/curl --netrc-file /etc/couchdb/couchdb.netrc ${host}/${name}"
+    cmd    => 'PUT',
+    host   => $host,
+    url    => $name,
+    unless => "/usr/bin/curl -s --netrc-file /etc/couchdb/couchdb.netrc ${host}/${name} | grep -q -v '{\"error\":\"not_found\"'"
   }
 
   couchdb::query { "db_security_${name}":
