b482ca211bf3e10a628f40e27a8321af723c51fb
varac <varacanero@zeromail.org>
2013-09-04 21:46:34 +0200

    linted

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -5,8 +5,9 @@ class couchdb::debian inherits couchdb::base {
   }
 
   file { '/etc/init.d/couchdb':
-    source  => [ 'puppet:///modules/site_couchdb/Debian/couchdb',
-                 'puppet:///modules/couchdb/Debian/couchdb' ],
+    source  => [
+      'puppet:///modules/site_couchdb/Debian/couchdb',
+      'puppet:///modules/couchdb/Debian/couchdb' ],
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
