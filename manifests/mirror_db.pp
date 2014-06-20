630ae0d3a9e80c189f485b3e79fbbe0aecd21d7c
Azul <azul@riseup.net>
2014-06-20 18:08:02 +0200

    allow setting custom netrc in query and document defines
    We need to perform some actions as the replication user for plain couch.

--- manifests/mirror_db.pp
+++ manifests/mirror_db.pp
@@ -3,14 +3,18 @@ define couchdb::mirror_db (
   $from='',
   $to='' )
 {
-  $source = "$from$name"
-  $target = "$to$name"
+  $source = "$from/$name"
+  if $to == '' { $target = $name }
+  else { $target = "$to/$name" }
+
+  $replication_user = "replication"
+  $replication_role = "replication"
 
   couchdb::document { "${name}_replication":
-    db   => "_replicate",
+    db   => "_replicator",
     id   => "${name}_replication",
     host => $host,
-    data   => "{ \"source\": ${source}, \"target\": ${target}, \"continuous\": true }",
+    data   => "{ \"source\": \"${source}\", \"target\": \"${target}\", \"continuous\": true, \"user_ctx\": { \"name\": \"${replication_user}\", \"roles\": [\"${replication_role}\"] } }",
     require => Couchdb::Query["create_db_${name}"]
   }
 }
